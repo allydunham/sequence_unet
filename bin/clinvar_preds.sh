1c52909348d2f79cfd4f99a0f430fa8b482e5450
allydunham <ally@ebi.ac.uk>
2021-06-21 12:29:42 +0100

    Added finetune models

--- bin/clinvar_preds.sh
+++ bin/clinvar_preds.sh
@@ -3,14 +3,22 @@
 # Predictions are generated for all clinvar variants for all models,
 # meaning models that were trained on clinvar also predict training data
 
+# UNET Models
 python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/size/f48_k9_l6/model.tf > data/clinvar/preds/unet_freq.tsv
 
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/structure/elu_32/model.tf > data/clinvar/preds/unet_freq_structure.tsv
 
+# Top Models
 python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/top_model/features/pred_freq_features_small/model.tf > data/clinvar/preds/unet_freq_features_top.tsv
 
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/top_model/features/pred_freq_structure_features_small/model.tf > data/clinvar/preds/unet_freq_structure_features_top.tsv
 
+# Finetune Models
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/top_model/finetune/classifier_sequence/model.tf > data/clinvar/preds/unet_freq_finetune.tsv
+
+python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/top_model/finetune/classifier_structure/model.tf > data/clinvar/preds/unet_freq_structure_finetune.tsv
+
+# Baseline Models
 python bin/predict.py --layers 0 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/baseline/freq_classifier/single/model.h5 > data/clinvar/preds/baseline_freq.tsv
 
 python bin/predict.py --layers 0 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/baseline/clinvar/single/model.h5 > data/clinvar/preds/baseline_clinvar.tsv
