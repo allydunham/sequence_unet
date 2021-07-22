50143d051140bcffbb47ee5a514414b0c96289db
allydunham <ally@ebi.ac.uk>
2021-07-22 17:37:27 +0100

    Switched to f64 as standard

--- bin/proteinnet_preds.sh
+++ bin/proteinnet_preds.sh
@@ -2,8 +2,8 @@
 # Make predictions for models against ProteinNet datasets
 
 # Sequence UNET Frequency Classification Predictions
-python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/testing models/classifier/size/f48_k9_l6/model.tf > data/freq/unet_sequence_testing.tsv
-python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/validation models/classifier/size/f48_k9_l6/model.tf > data/freq/unet_sequence_validation.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/testing models/classifier/size/f64_k9_l6/model.tf > data/freq/unet_sequence_testing.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/validation models/classifier/size/f64_k9_l6/model.tf > data/freq/unet_sequence_validation.tsv
 
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/testing models/classifier/structure/elu_32/model.tf > data/freq/unet_structure_testing.tsv
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/validation models/classifier/structure/elu_32/model.tf > data/freq/unet_structure_validation.tsv
@@ -12,8 +12,8 @@ python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/testing mod
 python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/validation models/baseline/freq_classifier/single/model.tf > data/freq/baseline_validation.tsv
 
 # Sequence UNET PSSM Predictions
-python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/testing models/pssm/size/f48_k9/model.tf > data/pssm/unet_sequence_testing.tsv
-python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/validation models/pssm/size/f48_k9/model.tf > data/pssm/unet_sequence_validation.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/testing models/pssm/size/f64_k9/model.tf > data/pssm/unet_sequence_testing.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/validation models/pssm/size/f64_k9/model.tf > data/pssm/unet_sequence_validation.tsv
 
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/testing models/pssm/structure/32/model.tf > data/pssm/unet_structure_testing.tsv
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/validation models/pssm/structure/32/model.tf > data/pssm/unet_structure_validation.tsv
