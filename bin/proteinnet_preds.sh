3522851036eea35be712bd8039ea59481a24ba4b
allydunham <ally@ebi.ac.uk>
2021-07-22 15:02:01 +0100

    Added baseline freq preds

--- bin/proteinnet_preds.sh
+++ bin/proteinnet_preds.sh
@@ -8,6 +8,9 @@ python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/validation 
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/testing models/classifier/structure/elu_32/model.tf > data/freq/unet_structure_testing.tsv
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/validation models/classifier/structure/elu_32/model.tf > data/freq/unet_structure_validation.tsv
 
+python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/testing models/baseline/freq_classifier/single/model.tf > data/freq/baseline_testing.tsv
+python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/validation models/baseline/freq_classifier/single/model.tf > data/freq/baseline_validation.tsv
+
 # Sequence UNET PSSM Predictions
 python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/testing models/pssm/size/f48_k9/model.tf > data/pssm/unet_sequence_testing.tsv
 python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/validation models/pssm/size/f48_k9/model.tf > data/pssm/unet_sequence_validation.tsv
