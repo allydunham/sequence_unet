46a1bc711581b99aee25e6ad1aa4c522ef6bd72b
allydunham <ally@ebi.ac.uk>
2021-07-22 19:22:41 +0100

    Corrected pssm baseline model path

--- bin/proteinnet_preds.sh
+++ bin/proteinnet_preds.sh
@@ -18,8 +18,8 @@ python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/validation 
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/testing models/pssm/structure/32/model.tf > data/pssm/unet_structure_testing.tsv
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/validation models/pssm/structure/32/model.tf > data/pssm/unet_structure_validation.tsv
 
-python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/testing models/baseline/freq_classifier/single/model.h5 > data/pssm/baseline_testing.tsv
-python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/validation models/baseline/freq_classifier/single/model.h5 > data/pssm/baseline_validation.tsv
+python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/testing models/baseline/pssm/single/model.h5 > data/pssm/baseline_testing.tsv
+python bin/predict.py --layers 1 --proteinnet data/proteinnet/casp12/validation models/baseline/pssm/single/model.h5 > data/pssm/baseline_validation.tsv
 
 # ClinVar Model Predictions
 python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/testing models/top_model/features/pred_freq_features_small/model.tf > data/clinvar/preds/pn_testing_features.tsv
