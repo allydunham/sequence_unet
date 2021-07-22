d3b829e7ffe86c7203fd8d3b340f268b2f759007
allydunham <ally@ebi.ac.uk>
2021-07-22 15:13:44 +0100

    Added thresholds

--- bin/clinvar_preds.sh
+++ bin/clinvar_preds.sh
@@ -4,9 +4,15 @@
 # meaning models that were trained on clinvar also predict training data
 
 # UNET Models
-python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/size/f48_k9_l6/model.tf > data/clinvar/preds/unet_freq.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/size/f64_k9_l6/model.tf > data/clinvar/preds/unet_freq.tsv
 
 python bin/predict.py --layers 6 --contacts --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/structure/elu_32/model.tf > data/clinvar/preds/unet_freq_structure.tsv
+
+# Thresholds
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/threshold/t0.1/model.tf > data/clinvar/preds/unet_freq_0.1.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/threshold/t0.01/model.tf > data/clinvar/preds/unet_freq_0.01.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/threshold/t0.001/model.tf > data/clinvar/preds/unet_freq_0.001.tsv
+python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/classifier/threshold/t0.0001/model.tf > data/clinvar/preds/unet_freq_0.0001.tsv
 
 # Top Models
 python bin/predict.py --layers 6 --proteinnet data/proteinnet/casp12/clinvar --clinvar --tsv data/clinvar/clinvar_variants.tsv models/top_model/features/pred_freq_features_small/model.tf > data/clinvar/preds/unet_freq_features_top.tsv
