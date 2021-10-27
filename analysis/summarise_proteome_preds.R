b86dbeef6ec74709ca69e67cafd0c24550cb0ba6
allydunham <ally@ebi.ac.uk>
2021-10-27 14:12:46 +0100

    Proteome summary based on tidyverse

--- analysis/summarise_proteome_preds.R
+++ analysis/summarise_proteome_preds.R
@@ -4,12 +4,28 @@ library(tidyverse)
 
 input_files <- commandArgs(TRUE)
 
-process_file <- function(x) {
-  df <- read_tsv(x)
-  group_by(df, gene) %>% summarise(across(A:Y, mean)) %>% mutate(overall = rowMeans(select(., A:Y)))
+process_gene <- function(x, ...) {
+  long <- pivot_longer(x, A:Y, names_to = "mut", values_to = "pred")
+  
+  mat <- group_by(long, wt, mut) %>%
+    summarise(m = mean(pred), .groups = "drop") %>%
+    mutate(pair = str_c(wt, mut, sep = "_")) %>%
+    select(pair, m) %>%
+    pivot_wider(names_from = pair, values_from = m)
+  
+  tibble(length = max(x$position),
+         mean_pred = mean(long$pred),
+         mean_wt = mean(long$pred[long$mut == long$wt]),
+         mean_mut = mean(long$pred[!long$mut == long$wt])) %>%
+    bind_cols(mat)
 }
 
-map_df(input_files, process_file) %>%
-  bind_rows() %>%
-  format_tsv() %>%
-  cat()
+process_file <- function(x, print_col_names=FALSE) {
+  read_tsv(x) %>%
+    group_by(gene) %>%
+    group_modify(process_gene) %>%
+    format_tsv(col_names = print_col_names) %>%
+    cat()
+}
+
+walk(input_files, ~process_file(., print_col_names = . == input_files[1]))
