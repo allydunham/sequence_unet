1c30c42a89b61a2d1f4a181ffd224bfdab04f04c
allydunham <ally@ebi.ac.uk>
2021-10-27 10:13:58 +0100

    Make list of proteins for proteomes

--- analysis/get_proteomes.R
+++ analysis/get_proteomes.R
@@ -2,15 +2,12 @@
 # Identify proteomes to fetch from Uniprot
 source("src/config.R")
 
-uniprot <- read_tsv("data/abundance/uniprot_proteomes", col_names = c("uniprot_proteome", "name", "tax_id", "proteins"))
-
-muller <- readxl::read_xlsx("data/abundance/muller_species.xlsx", range = "A2:C131", col_names = c("kingdom", "name", "tax_id"))
-
-cross <- filter(uniprot, tax_id %in% muller$tax_id) %>% 
-  group_by(tax_id) %>%
-  filter(proteins == max(proteins)) %>%
-  ungroup() %>%
-  distinct(tax_id, .keep_all = TRUE)
-
-proteomics <- read_csv("data/abundance/muller_proteomics.csv", col_names = c("row", "proteins", "intensity", "organism")) %>%
+proteomics <- read_csv("data/abundance/muller_proteomics.csv", col_names = c("row", "proteins", "intensity", "organism"), skip = 1) %>%
   select(-row)
+
+uniprot <- str_split(proteomics$proteins, ";") %>%
+  flatten_chr() %>%
+  unique() %>%
+  sort()
+
+cat(uniprot, file = "data/abundance/muller_uniprot_ids", sep = "\n")
