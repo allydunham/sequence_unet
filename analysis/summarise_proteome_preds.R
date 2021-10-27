e93ecc04ed29d9a37b05b42384383c4d876d08cb
allydunham <ally@ebi.ac.uk>
2021-10-27 18:23:54 +0100

    Switched to data.table summary script

--- analysis/summarise_proteome_preds.R
+++ analysis/summarise_proteome_preds.R
@@ -1,31 +1,33 @@
 #!/usr/bin/env Rscript
 # Summarise wide Sequence UNET prediction files
-library(tidyverse)
+library(data.table)
+library(purrr)
+
+amino_acids <- c("A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y")
+
+exp_pairs <- gtools::permutations(20, 2, v = amino_acids, repeats.allowed = TRUE)
+exp_pairs <- paste(exp_pairs[,1], exp_pairs[,2], sep = "_")
+
+exp_mat <- data.table(pair = exp_pairs)
 
 input_files <- commandArgs(TRUE)
 
 process_gene <- function(x, ...) {
-  long <- pivot_longer(x, A:Y, names_to = "mut", values_to = "pred")
+  long <- melt(x, measure.vars = amino_acids, variable.name = "mut", value.name = "pred")
+  mat <- long[, .(pair = paste0(wt, "_", mut), mean = mean(pred)), keyby = .(wt, mut)][,.(pair, mean)]
+  mat <- merge(mat, exp_mat, all = TRUE)   
   
-  mat <- group_by(long, wt, mut) %>%
-    summarise(m = mean(pred), .groups = "drop") %>%
-    mutate(pair = str_c(wt, mut, sep = "_")) %>%
-    select(pair, m) %>%
-    pivot_wider(names_from = pair, values_from = m)
-  
-  tibble(length = max(x$position),
-         mean_pred = mean(long$pred),
-         mean_wt = mean(long$pred[long$mut == long$wt]),
-         mean_mut = mean(long$pred[!long$mut == long$wt])) %>%
-    bind_cols(mat)
+  out <- cbind(data.table(length = nrow(x),
+                   mean_pred = mean(long$pred),
+                   mean_wt = mean(long$pred[long$mut == long$wt]),
+                   mean_mut = mean(long$pred[!long$mut == long$wt])),
+         dcast(mat, formula = . ~ pair, value.var = "mean"))
 }
 
 process_file <- function(x, print_col_names=FALSE) {
-  read_tsv(x) %>%
-    group_by(gene) %>%
-    group_modify(process_gene) %>%
-    format_tsv(col_names = print_col_names) %>%
-    cat()
+  dt <- fread(x, sep = "\t")
+  s <- dt[, process_gene(.SD), by = gene]
+  fwrite(s, quote = FALSE, sep = "\t", col.names = print_col_names)
 }
 
 walk(input_files, ~process_file(., print_col_names = . == input_files[1]))
