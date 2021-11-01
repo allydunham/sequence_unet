29a63530de4976c7b16ab45aad88d44e91862231
allydunham <ally@ebi.ac.uk>
2021-11-01 22:51:54 +0000

    Added option to use a lower than threshold

--- bin/summarise_proteome_preds.R
+++ bin/summarise_proteome_preds.R
@@ -10,20 +10,24 @@ parser <- arg_parser("Summarise wide Sequence UNET prediction files")
 parser <- add_argument(parser, "--files", help="Files to summarise", nargs = Inf, type="character")
 parser <- add_argument(parser, "--mut_thresh", help="Number of deleterious variants required for a position to be conserved", default=9)
 parser <- add_argument(parser, "--del_thresh", help="Deleteriousness threshold", default=0.5)
+parser <- add_argument(parser, "--del_less", short="-l", help="Deleteriousness threshold", flag=TRUE)
 args <- parse_args(parser)
 
-input_files <- commandArgs(TRUE)
+if (args$del_less) {
+  mut_del <- function(x) x < args$del_thresh
+} else {
+  mut_del <- function(x) x > args$del_thresh
+}
 
-# Guess thresholds from file name, defaulting
 
 process_gene <- function(x, ...) {
   long <- melt(x, measure.vars = amino_acids, variable.name = "mut", value.name = "pred")
-  pos_summary <- long[!wt == mut, .(mean_pred = mean(pred), n_conserved = sum(pred > args$del_thresh)), keyby = position]
+  pos_summary <- long[!wt == mut, .(mean_pred = mean(pred), n_conserved = sum(mut_del(pred))), keyby = position]
   
   data.table(mean_pred = mean(long$pred),
              mean_wt = mean(long$pred[long$mut == long$wt]),
              mean_mut = mean(long$pred[!long$mut == long$wt]),
-             percent_avg_conserved = sum(pos_summary$mean_pred > args$del_thresh) / nrow(x),
+             percent_avg_conserved = sum(mut_del(pos_summary$mean_pred)) / nrow(x),
              percent_n_conserved = sum(pos_summary$n_conserved > args$mut_thresh) / nrow(x))
 }
 
