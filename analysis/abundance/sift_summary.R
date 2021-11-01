6f3546277e8f33c5a0fa0269351ab62cc5428019
allydunham <ally@ebi.ac.uk>
2021-11-01 19:04:23 +0000

    Switched sift summary to data.table

--- analysis/abundance/sift_summary.R
+++ analysis/abundance/sift_summary.R
@@ -1,19 +1,17 @@
 #!/usr/bin/env Rscript
 # Summarise SIFT4G results
-library(tidyverse)
+library(data.table)
 
-bind_rows(
-  `Homo sapiens`=read_tsv("/nfs/research1/beltrao/ally/mutations/data/mutfunc/human/conservation/sift_parsed_all.tab", comment = "#"),
-  `Saccharomyces cerevisiae`=read_tsv("/nfs/research1/beltrao/ally/mutations/data/mutfunc/yeast/conservation/sift_parsed_all.tab", comment = "#"),
-  `Escherichia coli`=read_tsv("/nfs/research1/beltrao/ally/mutations/data/mutfunc/ecoli/conservation/sift.tab", comment = "#"),
-  .id = "organism"
-) %>%
-  group_by(acc, pos) %>%
-  summarise(mean_score = mean(score[ref != alt]),
-            n_conserved = sum(score[ref != alt] < 0.05),
-            .groups = "drop_last") %>%
-  summarise(mean_sift = mean(mean_score),
-            percent_avg_conserved = sum(mean_score < 0.05) / n(),
-            mean_conserved = mean(n_conserved),
-            percent_n_conserved = sum(n_conserved > 9) / n()) %>%
-  write_tsv("data/abundance/mutfunc_sift_summary.tsv")
+sift <- rbind(
+  `Homo sapiens`=fread("/nfs/research1/beltrao/ally/mutations/data/mutfunc/human/conservation/sift_parsed_all.tab", sep="\t", skip="acc"),
+  `Saccharomyces cerevisiae`=fread("/nfs/research1/beltrao/ally/mutations/data/mutfunc/yeast/conservation/sift_parsed_all.tab", sep="\t", skip="acc"),
+  `Escherichia coli`=fread("/nfs/research1/beltrao/ally/mutations/data/mutfunc/ecoli/conservation/sift.tab", sep="\t", skip="acc"),
+  .idcol = "organism"
+)
+
+pos_summary <- sift[ref != alt, .(mean_score = mean(score), n_conserved = sum(score < 0.05)), by = .(acc, pos)]
+overall_summary <- pos_summary[, .(mean_sift = mean(mean_score),
+                                   percent_avg_conserved = sum(mean_score < 0.05) / .N,
+                                   mean_conserved = mean(n_conserved),
+                                   percent_n_conserved = sum(n_conserved > 9) / .N), acc]
+fwrite(overall_summary, "data/abundance/mutfunc_sift_summary.tsv", quote = FALSE, sep = "\t", col.names = print_col_names)
