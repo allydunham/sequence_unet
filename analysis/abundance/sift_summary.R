9c26c91a1a04ad3f17370207a9ea237c1512a516
allydunham <ally@ebi.ac.uk>
2021-11-02 11:45:36 +0000

    Corrected rbind args typo

--- analysis/abundance/sift_summary.R
+++ analysis/abundance/sift_summary.R
@@ -7,7 +7,7 @@ sift <- rbind(
   `Homo sapiens`=fread("/nfs/research1/beltrao/ally/mutations/data/mutfunc/human/conservation/sift_parsed_all.tab", sep="\t"),
   `Saccharomyces cerevisiae`=fread("/nfs/research1/beltrao/ally/mutations/data/mutfunc/yeast/conservation/sift_parsed_all.tab", sep="\t", skip=1),
   `Escherichia coli`=rename(fread("/nfs/research1/beltrao/ally/mutations/data/mutfunc/ecoli/conservation/sift.tab", sep="\t"), acc = protein),
-  .idcol = "organism"
+  idcol = "organism"
 )
 
 pos_summary <- sift[ref != alt, .(mean_score = mean(score), n_conserved = sum(score < 0.05)), by = .(organism, acc, pos)]
