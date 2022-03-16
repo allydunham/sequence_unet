3ceb838952c381d98ad0f5e2b89ac4b7314f24f5
Alistair Dunham <ally_dunham@hotmail.co.uk>
2022-03-16 13:22:27 +0000

    Moved DMS ROC to own script

--- analysis/jelier_roc.R
+++ analysis/jelier_roc.R
@@ -4,28 +4,6 @@ source('src/config.R')
 source("src/analysis.R")
 plots <- list()
 
-import_dms <- function() {
-  long <- read_tsv("data/dms/long_combined_mutational_scans.tsv") %>%
-    select(gene, position, wt, mut, SIFT4G=sift, FoldX = total_energy) %>%
-    mutate(gene = str_to_lower(str_remove_all(gene, " ")))
-  preds <- bind_rows(
-    `Baseline ClinVar` = read_tsv("data/dms/preds/baseline_clinvar.tsv"),
-    `Baseline Frequency` = read_tsv("data/dms/preds/baseline_freq.tsv"),
-    `UNET (Top)` = read_tsv("data/dms/preds/clinvar_top.tsv"),
-    `UNET (Finetune)` = read_tsv("data/dms/preds/clinvar_finetune.tsv"),
-    `UNET` = read_tsv("data/dms/preds/unet_freq.tsv"),
-    .id = "model"
-  ) %>% pivot_wider(names_from = model, values_from = pred)
-  
-  read_tsv("data/dms/test.tsv") %>%
-    select(-study) %>%
-    arrange(gene, position) %>%
-    pivot_longer(A:Y, names_to = "mut", values_to = "score") %>%
-    left_join(long, by = c("gene", "position", "wt", "mut")) %>%
-    left_join(preds, by = c("gene", "position", "wt", "mut")) %>%
-    mutate(class = score < -0.5)
-}
-
 import_jelier <- function() {
   base <- read_tsv("data/jelier/jelier_variants.tsv") %>%
     mutate(class = effect == "deleterious") %>%
@@ -45,7 +23,6 @@ import_jelier <- function() {
     left_join(base, ., by = c("gene", "position", "wt", "mut"))
 }
 
-dms_preds <- import_dms()
 jelier_preds <- import_jelier()
 
 jelier_roc <- pivot_longer(jelier_preds, c(-gene, -position, -wt, -mut, -effect, -class), names_to = "model", values_to = "pred") %>%
@@ -56,14 +33,6 @@ jelier_roc <- pivot_longer(jelier_preds, c(-gene, -position, -wt, -mut, -effect,
   mutate(model_auc = auc_labeled_model(model, auc))
 write_tsv(jelier_roc, "data/jelier/roc.tsv")
 
-dms_roc <- pivot_longer(dms_preds, c(-gene, -position, -wt, -mut, -score, -class), names_to = "model", values_to = "pred") %>%
-  group_by(model) %>%
-  group_modify(~calc_roc(.x, class, pred, greater = !(.y == "SIFT4G"), max_steps = 6000)) %>%
-  ungroup() %>%
-  arrange(desc(auc)) %>%
-  mutate(model_auc = auc_labeled_model(model, auc))
-write_tsv(dms_roc, "data/dms/roc.tsv")
-
 plots$jelier_roc <- ggplot(jelier_roc, aes(x = fpr, y = tpr, colour = model_auc)) + 
   geom_abline(slope = 1, linetype = 'dashed', colour = 'black') +
   geom_step(direction = "hv") +
@@ -71,18 +40,6 @@ plots$jelier_roc <- ggplot(jelier_roc, aes(x = fpr, y = tpr, colour = model_auc)
   scale_colour_brewer(type = 'qual', palette = 'Dark2', name = '')
 
 plots$jelier_pr <- ggplot(jelier_roc, aes(x = tpr, y = precision, colour = model)) + 
-  geom_hline(yintercept = 0.5, linetype = 'dashed', colour = 'black') +
-  geom_line() +
-  labs(x = 'Recall', y = 'Precision') +
-  scale_colour_brewer(type = 'qual', palette = 'Dark2', name = '')
-
-plots$dms_roc <- ggplot(dms_roc, aes(x = fpr, y = tpr, colour = model_auc)) + 
-  geom_abline(slope = 1, linetype = 'dashed', colour = 'black') +
-  geom_step(direction = "hv") +
-  labs(x = 'False Positive Rate', y = 'True Positive Rate') +
-  scale_colour_brewer(type = 'qual', palette = 'Dark2', name = '')
-
-plots$dms_pr <- ggplot(dms_roc, aes(x = tpr, y = precision, colour = model)) + 
   geom_hline(yintercept = 0.5, linetype = 'dashed', colour = 'black') +
   geom_line() +
   labs(x = 'Recall', y = 'Precision') +
