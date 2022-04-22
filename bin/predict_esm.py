a8f7156d95c686ad51ee107073a427e93052ddd1
Alistair Dunham <ally_dunham@hotmail.co.uk>
2022-04-22 14:27:25 +0100

    Initial full ESM1b prediction, with batching

--- bin/predict_esm.py
+++ bin/predict_esm.py
@@ -7,6 +7,17 @@ import argparse
 import torch
 import proteinnetpy
 
+def batch_proteinnet_data(data, batch_size=5):
+    """
+    Generator yielding batches of ProteinNet records
+    """
+    records = []
+    for record in data:
+        records.append(record)
+        if len(records) == batch_size:
+            yield records
+            records = []
+
 def main():
     """
     Run ESM1b on input Fasta and format into a single output file
@@ -18,28 +29,29 @@ def main():
     model.eval()
 
     if torch.cuda.is_available() and not args.nogpu:
-        model = model.cuda()
+        model.cuda()
         print("Transferred model to GPU", file=sys.stderr)
 
     filter_func = proteinnetpy.data.make_length_filter(max_length=1022)
-    proteinnet = proteinnetpy.data.ProteinNetDataset(path=args.proteinnet, filter_func=filter_func)
+    proteinnet = proteinnetpy.data.ProteinNetDataset(path=args.proteinnet, filter_func=filter_func,
+                                                     preload=False)
 
     print("id", "position", "wt", *proteinnetpy.record.AMINO_ACIDS,
-          *[f"rep{i}" for i in range(1279)], sep="\t", file=sys.stdout)
-    for record in proteinnet:
-        print(f"Predicting {record.id}", file=sys.stderr)
-        data = [(record.id, "".join(record.primary))]
-        _, _, batch_tokens = batch_converter(data)
+          *[f"rep{i}" for i in range(1,1281)], sep="\t", file=sys.stdout)
+
+    for i, records in enumerate(batch_proteinnet_data(proteinnet, batch_size=args.batch_size)):
+        print(f"Predicting batch {i}", file=sys.stderr)
+        esm_input = [(i.id, "".join(i.primary)) for i in records]
+        _, _, batch_tokens = batch_converter(esm_input)
 
         with torch.no_grad():
             results = model(batch_tokens, repr_layers=[33], return_contacts=True)
         token_representations = results["representations"][33]
 
-
-
-
-
-
+        for record, rep in zip(records, token_representations):
+            for i in range(len(record.primary)):
+                print(record.id, i + 1, record.primary[i], *record.evolutionary[:,i],
+                      *rep.numpy()[0,:], sep="\t", file=sys.stdout)
 
 def parse_args():
     """Process arguments"""
@@ -50,6 +62,8 @@ def parse_args():
 
     parser.add_argument('--no_gpu', '-n', action="store_true",
                         help="Prevent GPU usage even when available")
+
+    parser.add_argument('--batch_size', '-b', default=5, help="Batch size")
 
     return parser.parse_args()
 
