f0be23e8ef9160f1be64f8a14e7c27bce6866dd4
Alistair Dunham <ally_dunham@hotmail.co.uk>
2022-04-29 17:18:56 +0100

    Renamed ESM scripts and expanded direct predictor to take any ESM model

--- bin/predict_esm_logits.py
+++ bin/predict_esm_logits.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Make predictions from ESM1b top models
+Make softmaxed logit predictions from ESM models
 """
 import sys
 import argparse
@@ -11,17 +11,22 @@ import proteinnetpy
 
 from Bio import SeqIO
 
-def load_esm1v_model(n=1, use_gpu=True):
+MODEL_STRS = {
+    f"esm1v_{n}": f"esm1v_t33_650M_UR90S_{n}" for n in range(1,6)
+}
+MODEL_STRS["esm1b"] = "esm1b_t33_650M_UR50S"
+
+def load_esm_model(model, use_gpu=True):
     """
-    Load an ESM-1v model
+    Load an ESM model
     """
-    esm_model, alphabet = torch.hub.load("facebookresearch/esm:main", f"esm1v_t33_650M_UR90S_{n}")
+    esm_model, alphabet = torch.hub.load("facebookresearch/esm:main", model)
     batch_converter = alphabet.get_batch_converter()
     esm_model = esm_model.eval()
 
     if torch.cuda.is_available() and use_gpu:
         esm_model = esm_model.cuda()
-        print("Transferred ESM1b to GPU", file=sys.stderr)
+        print("Transferred ESM model to GPU", file=sys.stderr)
 
     return esm_model, alphabet, batch_converter
 
@@ -34,9 +39,9 @@ def main():
 
     print("model", "id", "position", "wt", *[f"pred_{i}" for i in proteinnetpy.record.AMINO_ACIDS],
           sep="\t", file=sys.stdout)
-    for model_num in args.models:
+    for model_key in args.models:
         # Load ESM1b
-        model, alphabet, batch_converter = load_esm1v_model(model_num, use_gpu=use_gpu)
+        model, alphabet, batch_converter = load_esm_model(MODEL_STRS[model_key], use_gpu=use_gpu)
         mut_idxs = [alphabet.get_idx(i) for i in proteinnetpy.record.AMINO_ACIDS]
 
         # Load Data
@@ -81,9 +86,12 @@ def main():
 
             for p in range(len(record)):
                 wt_idx = alphabet.get_idx(seq[p])
-                mut_scores = results[p + 1, mut_idxs] - results[p + 1, wt_idx]
+                if args.wt_norm:
+                    scores = results[p + 1, mut_idxs] - results[p + 1, wt_idx]
+                else:
+                    scores = results[p + 1, mut_idxs]
 
-                print(model_num, rec_id, p + 1, seq[p], *mut_scores,
+                print(model_key, rec_id, p + 1, seq[p], *scores,
                       sep="\t", file=sys.stdout)
 
         # Wipe model from GPU to clear space for next model
@@ -103,8 +111,11 @@ def parse_args():
     parser.add_argument('--input_type', '-i', default="fasta", choices=["proteinnet", "fasta"],
                         help="Input file type")
 
-    parser.add_argument('--models',  '-m', nargs="+", default=[1], choices=[1, 2, 3, 4, 5],
-                        type=int, help="ESM-1v model number(s)")
+    parser.add_argument('--models', '-m', nargs="+", default=["esm1b"], type=str,
+                        choices=list(MODEL_STRS.keys()), help="ESM models to predict with")
+
+    parser.add_argument('--wt_norm', '-w', action="store_true",
+                        help="Normalise logits to WT amino acid")
 
     parser.add_argument('--no_gpu', '-n', action="store_true",
                         help="Prevent GPU usage for ESM1b even when available")
