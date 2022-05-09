9a7c038dd5070c030e6e5ce37b881521fd100323
Alistair Dunham <ally_dunham@hotmail.co.uk>
2022-05-09 12:39:35 +0100

    Added figure S1 and figure 2_3
    * Figure S1 contains paramater optimisation
    * Figure 2_3 is a combined summary of 2 and 3 for the main text when they are moved to supplementary in the reduced draft

---
+++ analysis/figures/figure2_3.R
@@ -0,0 +1,59 @@
+#!/usr/bin/env Rscript
+# Produce combined figure 2/3 - PSSM and classification performance
+source('src/config.R')
+source("src/analysis.R")
+
+### Panel - Correlation with true values ###
+pssm_models <- read_tsv("data/pssm/combined_preds.tsv") %>%
+  mutate(model = factor(model, levels = c("BLOSUM62", "SPBuild", "ESM-1b Logits", "ESM-1b Top Model", "Baseline CNN", "UNET", "PreGraph UNET")))
+
+pssm_cor <- group_by(pssm_models, model) %>%
+  group_modify(~broom::tidy(cor.test(.$pred, .$true)))
+
+p_pssm_cor <- ggplot(pssm_cor, aes(x = model, y = estimate, fill = model, ymin = conf.low, ymax = conf.high)) +
+  geom_col(width = 0.6, show.legend = FALSE) +
+  geom_errorbar(width = 0.5) +
+  coord_flip() +
+  scale_fill_manual(name = 'Model', values = TOOL_COLOURS) +
+  scale_y_continuous(expand = expansion(0), limits = c(0, 0.5)) +
+  labs(y = expression("Pearson's"~rho)) +
+  theme(axis.title.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(colour = "grey", linetype = "dotted"))
+
+### Panel - ROC curve ###
+classifier_roc <- read_tsv("data/freq/roc.tsv") %>%
+  group_by(model) %>%
+  mutate(pr_auc = pr_auc(tpr, precision)) %>%
+  ungroup() %>%
+  distinct(model, auc, pr_auc) %>%
+  mutate(model = factor(model, levels = model[order(auc)])) %>%
+  pivot_longer(c(auc, pr_auc), names_to = "metric", values_to = "value")
+
+p_roc <- ggplot(classifier_roc, aes(x = model, y = value, fill = model)) +
+  facet_wrap(~metric, labeller = labeller(metric = c(auc="ROC AUC", pr_auc="PR AUC")), strip.position = "bottom") +
+  geom_col(width = 0.6, show.legend = FALSE) +
+  coord_flip() +
+  scale_colour_manual(name = "", values = TOOL_COLOURS[names(TOOL_COLOURS) %in% model_names$model]) +
+  theme(strip.placement = "outside",
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line(colour = "grey", linetype = "dotted"))
+
+### Figure Assembly ###
+size <- theme(text = element_text(size = 10))
+p1 <- p_pssm_cor + labs(tag = 'A') + size
+p2 <- p_roc + labs(tag = 'B') + size
+
+figure2_3 <- multi_panel_figure(width = 120, height = c(60, 60), columns =  1,
+                              panel_label_type = 'none', row_spacing = 0, column_spacing = 0) %>%
+  fill_panel(p1, row = 1, column = 1) %>%
+  fill_panel(p2, row = 2, column = 1)
+
+ggsave('figures/figures/figure2_3.pdf', figure2_3, width = figure_width(figure2_3), height = figure_height(figure2_3),
+       units = 'mm', device = cairo_pdf())
+ggsave('figures/figures/figure2_3.png', figure2_3, width = figure_width(figure2_3), height = figure_height(figure2_3),
+       units = 'mm', dpi = 600)
---
+++ analysis/figures/figureS1.R
@@ -0,0 +1,84 @@
+#!/usr/bin/env Rscript
+# Generate figure S1 - Hyperparameter optimisation
+source('src/config.R')
+source("src/analysis.R")
+
+#### Panel - Training ####
+training_logs <- read_tsv("data/freq/training_logs.tsv") %>%
+  extract(model, into = c("experiment", "model", "dataset"), regex = "models/classifier/([^/]*)/([^/]*)/([^/]*)")
+
+# Calculate variance over replicates for text
+# filter(training_logs, experiment == "replicates") %>% 
+#   filter(step == max(step)) %>% 
+#   group_by(dataset, metric) %>% 
+#   summarise(mean = mean(value), var = var(value))
+
+p_train_activation <- filter(training_logs, experiment == "activation", metric == "epoch_masked_accuracy", dataset == "validation") %>%
+  group_by(model) %>%
+  filter(step == step[which.max(value)]) %>%
+  ungroup() %>%
+  ggplot(aes(x = reorder(model, value), y = value)) +
+  geom_point(shape = 20, colour = "#377eb8", size = 4) +
+  coord_flip(clip = "off") +
+  scale_x_discrete(labels = c(elu = "ELU", hard_sigmoid = "Hard Sigmoid", relu = "ReLU", swish = "Swish", tanh = "Tanh")) +
+  scale_y_continuous(limits = c(0.73, 0.75), labels = function(x) str_remove(x, "0*^")) +
+  labs(x = "", y = "Accuracy") +
+  theme(panel.grid.major.x = element_line(colour = "grey", linetype = "dotted"),
+        panel.grid.major.y = element_blank())
+
+training_log_size <- filter(training_logs, experiment == "size", dataset == "validation") %>%
+  group_by(model) %>%
+  filter(step == step[which.max(value)]) %>%
+  ungroup() %>%
+  extract(model, c("filters", "kernel_size", "layers"), "f([0-9]*)_k([0-9]*)_l([0-9]*)", convert = TRUE)
+
+p_train_filters <- filter(training_log_size, layers == 6, kernel_size == 9, metric == "epoch_masked_accuracy") %>%
+  ggplot(aes(x = filters, y = value)) +
+  geom_point(shape = 20, colour = "#4daf4a", size = 4) +
+  scale_x_continuous(breaks = c(32, 48, 64, 80, 96)) +
+  ylim(0.75, 0.76) +
+  coord_cartesian(clip = "off") +labs(x = "Base CNN Filters", y = "Accuracy")
+
+p_train_kernel <- filter(training_log_size, layers == 6, filters == 32, metric == "epoch_masked_accuracy") %>%
+  ggplot(aes(x = kernel_size, y = value)) +
+  geom_point(shape = 20, colour = "#984ea3", size = 4) +
+  scale_x_continuous(breaks = c(3,5,7,9)) +
+  coord_cartesian(clip = "off") +labs(x = "Kernel Size", y = "Accuracy")
+
+p_train_layers <- filter(training_log_size, filters == 32, kernel_size == 5, metric == "epoch_masked_accuracy") %>%
+  ggplot(aes(x = layers, y = value)) +
+  geom_point(shape = 20, colour = "#ff7f00", size = 4) +
+  scale_x_continuous(breaks = c(2,4,6)) +
+  coord_cartesian(clip = "off") +labs(x = "Layers", y = "Accuracy")
+
+p_train_structure <- filter(training_logs, experiment == "structure", metric == "epoch_masked_accuracy", dataset == "validation") %>%
+  group_by(model) %>%
+  filter(step == step[which.max(value)]) %>%
+  ungroup() %>%
+  separate(model, c("activation", "layers"), extra = "merge") %>%
+  mutate(nlayers = ifelse(layers == "none", 0, str_count(layers, "_") + 1)) %>%
+  filter(activation != "relu", str_starts(layers, "none|32")) %>%
+  ggplot(aes(x = nlayers, y = value)) +
+  geom_point(shape = 20, colour = "#a65628", size = 4) +
+  coord_cartesian(clip = "off", ylim = c(0.75, 0.775)) +
+  labs(x = "Graph Layers", y = "Accuracy")
+
+#### Figure Assembly ####
+size <- theme(text = element_text(size = 11))
+p1 <- p_train_activation + labs(tag = 'A') + size
+p2 <- p_train_filters + labs(tag = 'B') + size
+p3 <- p_train_kernel + labs(tag = 'C') + size
+p4 <- p_train_layers + labs(tag = 'D') + size
+p5 <- p_train_structure + labs(tag = 'E') + size
+
+figure <- multi_panel_figure(width = c(90, 90), height = c(50, 50, 50),
+                             panel_label_type = 'none', row_spacing = 0, column_spacing = 0) %>%
+  fill_panel(p1, row = 1, column = 1:2) %>%
+  fill_panel(p2, row = 2, column = 1) %>%
+  fill_panel(p3, row = 2, column = 2) %>%
+  fill_panel(p4, row = 3, column = 1) %>%
+  fill_panel(p5, row = 3, column = 2)
+
+ggsave('figures/figures/figure_S1.pdf', figure, width = figure_width(figure), height = figure_height(figure), units = 'mm',
+       device = cairo_pdf)
+ggsave('figures/figures/figure_S1.png', figure, width = figure_width(figure), height = figure_height(figure), units = 'mm')
